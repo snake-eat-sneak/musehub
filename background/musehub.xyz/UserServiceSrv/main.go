735f48214261a48dd77c3b72266be8ba4942fc90
ctwww <634008786@qq.com>
2020-10-18 05:30:33 +0800

    srv

--- background/musehub.xyz/UserServiceSrv/main.go
+++ background/musehub.xyz/UserServiceSrv/main.go
@@ -2,41 +2,36 @@
 package main
 
 import (
+	proto "UserServiceSRV/proto"
 	"context"
 	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-micro/util/log"
-	"github.com/micro/go-plugins/registry/consul"
-
-	proto "UserServiceSrv/proto"
 )
 
 type User struct{}
 
 func (s *User) UserLogin(ctx context.Context, req *proto.UserLoginRequest, rsp *proto.UserLoginResponse) error {
 	log.Log("Received Say.Hello request")
-	userInfo := make([]*proto.User,1,1)
-	userInfo = append(userInfo, &proto.User{UserId:req.UserId, Password:req.Password, Name:"ctwww_test"})
+	userInfo := make([]*proto.UserInfo,0,1)
+	userInfo = append(userInfo, &proto.UserInfo{UserId:req.UserId, Password:req.Password, Name:"ctwww_test"})
 	rsp.Code = 0
-	rsp.User = userInfo
+	rsp.UserInfo = userInfo
 	return nil
 }
 
 func main() {
-	consulReg := consul.NewRegistry(
-		registry.Addrs("127.0.0.1:8500"),
-	)
+	//etcdReg := etcd.NewRegistry(registry.Addrs("127.0.0.1:2379"))
 	service := micro.NewService(
 		micro.Name("srv.musehub.xyz.user"),
 		micro.Address(":8011"),
-		micro.Registry(consulReg),
+		//micro.Registry(etcdReg),
 	)
 
 	// optionally setup command line usage
 	service.Init()
 
 	// Register Handlers
-	proto.RegisterUserServiceSrvHandler(service.Server(), new(User))
+	proto.RegisterUserHandler(service.Server(), new(User))
 
 	// Run server
 	if err := service.Run(); err != nil {
--- background/musehub.xyz/UserServiceSrv/service_start.bat
+++ background/musehub.xyz/UserServiceSrv/service_start.bat
@@ -1,7 +1,5 @@
 @echo off
 
-start "prod1" go run UserServiceSRV --server_address :8101 &
-start "prod2" go run UserServiceSRV --server_address :8102 &
-start "prod3" go run UserServiceSRV --server_address :8103
+go run UserServiceSRV --registry=etcd --registry_address=localhost:2379
 
 pause
